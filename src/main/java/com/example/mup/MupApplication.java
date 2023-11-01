98d1442fb02ed829862a4262b9be0aee9f8eb7f9
Heeseok1210 <Heeseok1210@gmail.com>
2023-11-01 14:32:00 +0900

    Initial commit

---
+++ src/main/java/com/example/mup/MupApplication.java
@@ -0,0 +1,13 @@
+package com.example.mup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MupApplication.class, args);
+    }
+
+}
